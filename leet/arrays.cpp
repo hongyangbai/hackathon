165e39f2a36148ab7da24499157d609a8c3dcc7e
hongyangbai <hongyangbai@gmail.com>
2017-08-12 17:58:48 -0500

    firsst question solved

--- leet/arrays.cpp
+++ leet/arrays.cpp
@@ -4,7 +4,37 @@ using namespace std;
 
 int singleNonDuplicate(vector<int>& nums)
 {
-	int res = 0;
+	if(nums.empty())	return 0;
+	int l = 0;
+	int r = nums.size();
+	int i = (l+r)/2;
+
+	while(1)
+	{
+		i = (l+r)/2;
+		if(i == 0 && nums[i+1] != nums[i])
+			break;
+		else if(i == nums.size()-1 && nums[i-1] != nums[i])
+			break;
+		else if(i > 0 && i < (nums.size()-1) && (nums[i] != nums[i-1] && nums[i] != nums[i+1]))
+			break;
+
+		if(nums[i-1] == nums[i])
+		{
+			if((i-l) % 2 == 0)
+				r = i-2;
+			else
+				l = i+1;
+		}
+
+		if(nums[i+1] == nums[i])
+		{
+			if((i-l)%2 == 0)
+				l = i+2;
+			else
+				r = i-1;
+		}
+	}
 	
-	return res;
+	return nums[i];
 }
--- thor/thor.cpp
+++ thor/thor.cpp
@@ -6,7 +6,8 @@ int singleNonDuplicate(vector<int>& nums);
 
 int main(int argc, char *argv[])
 {	
-	vector<int> nums = {1,2,3};
+	//vector<int> nums = {1,1,2,3,3,4,4,8,8};
+	vector<int> nums = {3,3,7,7,10,11,11};
 	cout<<singleNonDuplicate(nums)<<endl;
 	return 0;
 }
